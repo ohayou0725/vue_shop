0ddac2da5f7a53309afdb7448ce3859992780310
llyyffhh <llyyffhh@gmail.com>
2020-07-03 22:18:48 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+//导入全局样式表
+import './assets/css/global.css'
+//导入icon
+import './assets/fonts/iconfont.css'
+import axios from "axios"
+axios.defaults.baseURL="http://timemeetyou.com:8889/api/private/v1/"
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
